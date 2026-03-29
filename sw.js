896d6af94121022e1a7b8e11d00b673862924968
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2026-03-29 22:12:54 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-const CACHE="dhikr-pro-cache"
-545
-const urls=[
-"/",
-"/index.html",
-"/style.css",
-"/script.js"
-]
+const CACHE = "dhikr-pro-premium-v1";
+const urls = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(cache=>{
-return cache.addAll(urls)
-})
-)
-})
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(urls);
+    })
+  );
+});
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(res=>{
-return res||fetch(e.request)
-})
-)
-})
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
